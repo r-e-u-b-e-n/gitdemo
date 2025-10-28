7f2597669faab06d1cc2cd702ef0de180594c3d2
Reuben <dsouzareuben79@gmail.com>
2025-10-28 09:44:10 +0530

    "printed counter"

--- lib/main.dart
+++ lib/main.dart
@@ -57,6 +57,8 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
+    print(_counter);
+
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
